8ff00d9ee4d2490ae6e5147895f8eccd8e809db1
crhis06 <cristopmejia090@gmail.com>
2024-08-04 15:39:20 -0500

    Se agrega el proyecto

---
+++ Loginjuego.py
@@ -0,0 +1,13 @@
+user_ = "Crhis06"
+pwd_ = "12345@"
+print ("Bienvenido, ingresa tu usuario")
+print ("Usuario: ")
+user = input ()
+
+print ("Contraseña: ")
+pwd = input ()
+
+if user==user_ and pwd==pwd_:
+    print ("Bienvenido de vuelta", user_)
+else :
+    print ("Usuario o contraseña incorrectos")
